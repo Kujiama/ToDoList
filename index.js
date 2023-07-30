a965193475d88aaaa0e006bba56038648f0a8724
Kujiama <englandpelenio17@gmail.com>
2023-07-30 15:27:53 -0400

    added new fumctionality

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const addTask = document.querySelector(".new-task") // get the button for adding
 const deleteTask = document.querySelector(".remove-task") // get the button for removing task
 
 // functions
+
+/* functions to addd
+
+    1. when clicking the check box i want the task to be striked out ✅
+    2. be able to delete the selected task 🚧
+
+*/
+
 function addNewTask(){ 
 
     let taskName = document.querySelector(".new-task-input").value; // we get the text inside input element
@@ -21,7 +29,7 @@ function addNewTask(){
         // set the innerHTML of the new task
         newTask.innerHTML = `
             <div class="new-task">
-                <input type="checkbox" class="new-task_checkbox">
+                <input type="checkbox" class="new-task_checkbox" onchange="finishedTask(this)">
                 <span class="new-task_title">${taskName}</span>
                 <button type="button" class="remove-task btn" onclick="deleteCurrTask()">Delete</button>
             </div>
@@ -29,18 +37,38 @@ function addNewTask(){
         
         tasks.appendChild(newTask); // add the new task to the unordered list
     }
+}
+
+function finishedTask(task){
+    let currTask = task.closest("li"); // we get the closest anscestor element of the checkbox
+    
+    if(currTask){ // if the li element exists
+        let taskName = currTask.querySelector(".new-task_title");
+        /*
+            Q -> why use currTask.querySelector instead of document.QuerySelector?
+
+            A -> document.querySelector has a broader scope which means that it can choose
+                outside of the current selected list element. On the other hand, currTask.querySelector's
+                scope is in only its children.
+        */
+
+        if(taskName && task.checked == true){
+            taskName.innerHTML = `<s>${taskName.textContent}</s>`;
+        }else{
+            taskName.innerHTML = taskName.textContent;
+        }
+    }
     
 }
 
 function deleteCurrTask(){
-    let currTask = document.querySelector(".new-task-wrapper") // we get the elemeent that has the selector
-
+    let currTask = document.querySelector(".new-task-wrapper") // we get the elemeent that has the selector 
     /*
         The current problem of this is that It will get the first element it finds.
         In this case it deletes the first task in the list no matter what delete button you press.
     */
 
-    if(currTask){ // checks if task item is found 
-        currTask.remove();
+    if(currTask){ // if task  is found 
+        currTask.remove(); // remove task
     }
 }
